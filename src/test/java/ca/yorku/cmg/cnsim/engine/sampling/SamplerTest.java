a6d8c17ff17e5e6c00aca7cae6729c23b15050d2
nina2dev <nina2dev@gmail.com>
2026-02-20 20:02:48 -0500

    test(sampler): add test for sampling
    - Add Sampler test design doc.
    
    % FOOTER %
    type-of: test
    type-of: docs
    
    addresses: https://esg-yorku-team.atlassian.net/browse/CE-186
    addresses-desc: Test and document Sampler - Nina

---
+++ src/test/java/ca/yorku/cmg/cnsim/engine/sampling/SamplerTest.java
@@ -0,0 +1,17 @@
+package ca.yorku.cmg.cnsim.engine.sampling;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * JUnit tests for {@link Sampler}.
+ *
+ * Test design document:
+ * src/test/resources/test-design/ca/yorku/cmg/cnsim/engine/sampling/SamplerTest.md
+ *
+ */
+public class SamplerTest {
+
+}
